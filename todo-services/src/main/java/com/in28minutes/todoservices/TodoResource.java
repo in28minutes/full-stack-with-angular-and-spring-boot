f68071816094e1ddad438294726f54e845dce6d3
Ranga Rao Karanam <in28minutes@gmail.com>
2018-11-08 20:32:48 +0530

    Thank You for Choosing to Learn from in28Minutes

--- todo-services/src/main/java/com/in28minutes/todoservices/TodoResource.java
+++ todo-services/src/main/java/com/in28minutes/todoservices/TodoResource.java
@@ -1,12 +1,17 @@
 package com.in28minutes.todoservices;
 
 import java.util.Date;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoResource {
+	
+	@Autowired
+	private TodoService todoService;
 	
 	@GetMapping("/todos/1")
 	public Todo retrieveTodo() {
@@ -15,9 +20,9 @@ public class TodoResource {
 	}
 	
 	@GetMapping("/todos")
-	public Todo retrieveTodos() {
-		return new Todo(1l,"in28minutes","Learn to Dance", 
-				new Date(), false);
+	public List<Todo> retrieveTodos() {
+		//TODO : HARDCODED in28minutes
+		return todoService.retrieveTodos("in28minutes");
 	}
 
 }
---
+++ todo-services/src/main/java/com/in28minutes/todoservices/TodoService.java
@@ -0,0 +1,64 @@
+package com.in28minutes.todoservices;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.in28minutes.todoservices.Todo;
+
+@Service
+public class TodoService {
+  
+  private static List<Todo> todos = new ArrayList<Todo>();
+  
+  private static long todoCount = 3;
+
+  static {
+    todos.add(new Todo(1l, "in28minutes", "Learn Spring MVC", new Date(),
+        false));
+    todos.add(new Todo(2l, "in28minutes", "Learn Struts", new Date(), false));
+    todos.add(new Todo(3l, "in28minutes", "Learn Hibernate", new Date(),
+        false));
+  }
+
+  public List<Todo> retrieveTodos(String username) {
+    List<Todo> filteredTodos = new ArrayList<Todo>();
+    for (Todo todo : todos) {
+      if (todo.getUsername().equals(username))
+        filteredTodos.add(todo);
+    }
+    return filteredTodos;
+  }
+
+  public Todo retrieveTodo(int id) {
+    for (Todo todo : todos) {
+      if (todo.getId() == id)
+        return todo;
+    }
+    return null;
+  }
+
+  public void updateTodo(Todo todo) {
+    todos.remove(todo);
+    todos.add(todo);
+  }
+
+  public Todo addTodo(String name, String desc, Date targetDate,
+      boolean isDone) {
+    Todo todo = new Todo(++todoCount, name, desc, targetDate, isDone);
+    todos.add(todo);
+    return todo;
+  }
+
+  public void deleteTodo(int id) {
+    Iterator<Todo> iterator = todos.iterator();
+    while (iterator.hasNext()) {
+      Todo todo = iterator.next();
+      if (todo.getId() == id) {
+        iterator.remove();
+      }
+    }
+  }
+}
