e9abece20d0d9615e58a956091ed4440e0eba9aa
bsmahi <bs.mahi@gmail.com>
2024-10-31 20:29:28 +0530

    Refactored to Angular 18

--- frontend/todo/src/main.ts
+++ frontend/todo/src/main.ts
@@ -1,24 +1,13 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
+import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { HttpIntercepterBasicAuthService } from './app/service/http/http-intercepter-basic-auth.service';
-import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, FormsModule),
-        { provide: HTTP_INTERCEPTORS, useClass: HttpIntercepterBasicAuthService, multi: true },
-        provideHttpClient(withInterceptorsFromDi())
-    ]
-})
+bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
